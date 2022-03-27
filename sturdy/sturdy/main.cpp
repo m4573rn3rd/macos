35fdd5229197e0534c7b908cb9c0cafcf30a244f
Brandon McClain <m4573rn3rd@gmail.com>
2022-03-26 22:23:04 -0500

    03262022

--- sturdy/sturdy/main.cpp
+++ sturdy/sturdy/main.cpp
@@ -1,14 +1,29 @@
 //
 //  main.cpp
-//  sturdy
+//  Testing Tool
 //
 //  Created by Brandon McClain on 3/26/22.
 //
 
 #include <iostream>
+#include "tool.hpp"
+#include "menu.hpp"
+#include <unistd.h>
+
+
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Booting Program . . .\n";
+    // Pause for five seconds.
+    sleep(5);
+    std::cout << "Press enter when ready.\n";
+    secondFunction();
     return 0;
+    
+    
 }
+
+
